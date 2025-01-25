d37b289f87e2f0ed64d424f9e0a4262b422151bc
Sergio <sergio.bianchi.2007@calvino.edu.it>
2025-01-25 12:07:04 +0100

    Direi che ho finito con i canva

--- Esercitazione 5/JS/script.js
+++ Esercitazione 5/JS/script.js
@@ -2,6 +2,8 @@ const canva_div = document.getElementById("DIV").getContext("2d")
 const canva_arr = document.getElementById("ARR").getContext("2d")
 const canva_lim = document.getElementById("LIM").getContext("2d")
 const canva_att = document.getElementById("ATT").getContext("2d")
+const canva_sal = document.getElementById("SAL").getContext("2d")
+const canva_stp = document.getElementById("STP").getContext("2d")
 
 
 canva_div.beginPath()
@@ -33,7 +35,6 @@ canva_div.beginPath()
 canva_div.moveTo(80, 220)
 canva_div.lineTo(220, 80)
 canva_div.stroke()
-
 
 
 canva_arr.beginPath()
@@ -89,25 +90,140 @@ canva_lim.stroke()
 canva_lim.beginPath()
 canva_lim.fillStyle = "black"
 canva_lim.font = "120px Arial";
-canva_lim.fillText("30", 85, 190)
-
-
+canva_lim.fillText("30", 78, 195)
 
 
 canva_att.beginPath()
 
-canva_att.moveTo(140, 40)
+canva_att.moveTo(150, 30)
 canva_att.lineTo(30, 270)
 canva_att.lineTo(30, 270)
-canva_att.bezierCurveTo(40, 270, 40, 290, 210, 140)
 
 canva_att.lineTo(270, 270)
-canva_att.lineTo(160, 40)
+canva_att.lineTo(150, 30)
+canva_att.fillStyle = "white"
+canva_att.closePath()
+canva_att.fill()
 
 
-canva_att.lineWidth = 10
-canva_att.stroke()
+canva_att.beginPath()
+
+canva_att.moveTo(150, 50)
+canva_att.lineTo(50, 260)
+
+canva_att.lineTo(250, 260)
+canva_att.lineTo(150, 50)
+canva_att.fillStyle = "red"
+canva_att.closePath()
+canva_att.fill()
+
+
+canva_att.beginPath()
+
+canva_att.moveTo(150, 90)
+canva_att.lineTo(80, 242)
+canva_att.lineTo(220, 242)
+canva_att.lineTo(150, 90)
+canva_att.fillStyle = "white"
+canva_att.closePath()
+canva_att.fill()
+
+canva_att.fillStyle = "black"
+canva_att.font = "140px Arial";
+canva_att.fillText("!", 110, 230)
+
+
+canva_sal.beginPath()
+
+canva_sal.moveTo(150, 30)
+canva_sal.lineTo(30, 270)
+canva_sal.lineTo(30, 270)
+
+canva_sal.lineTo(270, 270)
+canva_sal.lineTo(150, 30)
+canva_sal.fillStyle = "white"
+canva_sal.closePath()
+canva_sal.fill()
+
+
+canva_sal.beginPath()
+
+canva_sal.moveTo(150, 50)
+canva_sal.lineTo(50, 260)
+
+canva_sal.lineTo(250, 260)
+canva_sal.lineTo(150, 50)
+canva_sal.fillStyle = "red"
+canva_sal.closePath()
+canva_sal.fill()
+
+
+canva_sal.beginPath()
+
+canva_sal.moveTo(150, 90)
+canva_sal.lineTo(80, 242)
+canva_sal.lineTo(220, 242)
+canva_sal.lineTo(150, 90)
+canva_sal.fillStyle = "white"
+canva_sal.closePath()
+canva_sal.fill()
+
+canva_sal.beginPath()
+canva_sal.moveTo(85, 239)
+canva_sal.lineTo(215, 239)
+canva_sal.lineTo(108, 188)
+canva_sal.lineTo(85, 239)
+canva_sal.lineWidth = 1
+canva_sal.fillStyle = "black"
+canva_sal.closePath()
+canva_sal.fill()
+
+canva_sal.save();
+canva_sal.translate(120, 190);
+canva_sal.rotate(Math.PI / 6.8);
+
+canva_sal.font = "35px Arial";
+canva_sal.fillText("10%", 0, 0)
+canva_sal.rotate(Math.PI / 10);
+canva_sal.restore();
+
+canva_sal.stroke()
 
 
 
+
+canva_stp.beginPath();
+
+octagon(canva_stp, 150, 150, 130)
+
+canva_stp.closePath();
+canva_stp.fillStyle = "white"
+canva_stp.fill();
+
+
+canva_stp.beginPath();
+octagon(canva_stp, 150, 150, 120)
+canva_stp.closePath();
+canva_stp.fillStyle = "red"
+canva_stp.fill();
+
+canva_stp.fillStyle = "white"
+
+canva_stp.font = "80px Arial";
+canva_stp.fillText("STOP", 55, 180)
+
+
+function octagon(e, centerX, centerY, radius) {
+    for (let i = 0; i < 8; i++) {
+        const angle = (i * Math.PI) / 4 - Math.PI / 8;
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+
+        if (i === 0) {
+            e.moveTo(x, y);
+        } else {
+            e.lineTo(x, y);
+        }
+    }
+}
 
